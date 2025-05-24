694074f16d9fdb0ea8d03aeda46f6a3d3bf218a4
iqra328 <rehmaniqra22@gmail.com>
2025-05-24 06:26:40 -0700

    Add files via upload

---
+++ ass5/ass.js
@@ -0,0 +1,189 @@
+Q1
+// I'll give you one exercise at a time. After you give your answer for each exercise, I'll give you another exercise. When you get one wrong, I'll let you know. Later in the session, I'll circle back to that exercise to give you another try.
+// What is the keyword that displays a message box? 
+                
+                   alert
+
+Q2
+//Type the character that ends every statement.
+
+                          ;
+
+
+
+Q3
+// Type the 2 characters that are missing in this statement.
+// alert"You're learning JavaScript!";
+                            ()
+
+Q4
+// A __________ ends in a semicolon.
+                    
+                   statement
+
+
+
+Q5
+// Drag-and-drop. Holding down the left mouse button, drag the pieces of code onto the screen to form an alert.
+// Hi there, Sailor! alert( ) " " ;
+
+
+                  alert("Hi there, Sailor!");
+
+
+q6
+// Click in the box and type the next character. I'll autocomplete. Don't type spaces.
+
+// Code an alert that displays the message "Thanks for everything!"
+                        alert("Thanks for everything!");
+
+
+
+q7
+// The code displays a box with a message in it. Fill in the blank.
+// ______("You won!");
+                    alert
+
+
+Q8
+// The code displays a box with a message in it. Fill in the blank.
+// alert("You won!____
+                         ("you won!");
+
+
+
+Q9
+// Code an alert that displays the message "Hi"
+               alert("hi");
+
+
+
+
+Q10
+// What are the 5 punctuation characters of an alert statement, in order?
+
+                       ("");
+
+
+Q11
+// Code an alert statement displaying any message you like.
+
+                  alert("By");
+  
+
+
+Q12
+// Time goal: 12 seconds. (The timer starts when you click in the answer field.) The code displays a box with a message in it. Fill in the blank.
+// ______("You won!");
+
+                   alert
+
+Q13
+// Time goal: 10 seconds. The code displays a box with a message in it. Fill in the blank.
+// alert("You won!____
+
+
+                  ("");
+
+Q14
+// Time goal: 20 seconds. Code an alert that displays the message "Hi"
+
+                 alert("Hi");
+
+Q15
+// Time goal: 20 seconds. What are the 5 punctuation characters of an alert statement, in order?
+
+
+                    ("");
+
+Q16
+// Time goal: 25 seconds. Code an alert statement displaying any message you like.
+
+                       alert("parrot");
+
+Q17
+// Time goal: 25 seconds. Code an alert statement that displays your full name.
+                        alert("Iqra Rehman");
+
+
+Q18
+// Live coding exercise: Code an alert with the message "Hello world!" Then click the Run It button to run your code live. Type out the code. If you copy and paste it from the screen showing the correct code, it may not work.
+
+
+                         alert("Hello world!");
+                    
+
+Q19
+// Live coding exercise: Code an alert with any message you like. Then click the Run It button to run your code live. Type out the code. If you copy and paste it from the screen showing the correct code, it may not work.
+
+                             alert("WHAT");
+
+
+Q20
+// Click in the box and type the next character. I'll autocomplete. Don't type spaces.
+
+// Code an alert that displays the message "Thanks for everything!"
+
+
+                            alert("Thanks for everything!");
+
+
+Q21
+//   Time goal: 25 seconds. Code an alert statement displaying any message you like.
+
+
+                               alert("by");
+
+
+                      
+                   
+                   
+
+
+
+                  
+
+                      
+
+
+
+
+ 
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
